048994c493007de8d9e33a37dfd8da5dd1bad9a7
flrnllpnt <71372305+flrnllpnt@users.noreply.github.com>
2020-10-06 10:09:38 +0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+package com.pboreg;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        // untuk mencetak angka ganjil i = 1
+        // untuk menambahkan angka genap i = 0
+        for (int i = 0; i <= 10; i = i+2){
+            System.out.print(i + " ");
+        // i + "(spasi)" agar ada spasi di tampilannya
+        }
+    }
+}
